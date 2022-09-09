feb73b3f70b9add05d440834db79a3b4ea412196
yewcm123 <yewcm123@gmail.com>
2022-09-09 13:56:10 +0800

    Optimising

--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -1,8 +1,20 @@
 import React, { useState } from 'react'
+import Header from '../components/Header'
+import LineChart from '../components/charts/LineChart'
+
+const DispBlock = () =>{
+  
+}
 
 const Overview = () => {
   return (
-    <div>Overview</div>
+    <div className='bg-white h-full m-5 p-2 pt-3 border-round-3xl'>
+        <Header category='Main' title='Overview'/>
+        <div className='m-4'>
+            
+        </div>
+        
+    </div>
   )
 }
 
